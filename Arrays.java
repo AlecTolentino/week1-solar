18337c83bddf0b5b2b2885461bb20889711c93a9
Tolentino <tolentia@vdi-scclab-013.lancs.local>
2023-10-19 19:44:51 +0100

    Completed saturns rings with movement, added more moons

--- Arrays.java
+++ Arrays.java
@@ -7,8 +7,13 @@ public class Arrays extends Moon {
 
         super(theSolarSystem, myPlanet, distance, angle, diameter, colour, centreRotateDis, centreRotateAng, velocity, rotVelocity );
         this.asteroidBelt =  new Asteroid[30];
-        this.saturnsRings = new Asteroid[10];
+        this.saturnsRings = new Asteroid[600];
 
+    }
+
+    public Asteroid[] getRings() {
+
+        return saturnsRings;
     }
 
     public void addAsteroid(Asteroid newAsteroid) {
@@ -24,26 +29,40 @@ public class Arrays extends Moon {
         
     }
 
-    public void addRing(Asteroid newRing) {
+    public void addRing(Asteroid newRing, Asteroid newRing2, Asteroid newRing3) {
 
-        
+        newRing.setAngle(newRing.getAngle() + newRing.getVelocity());
+        newRing.setRotAngle(newRing.getRotAng() + newRing.getRotVelocity());
+
+        newRing2.setAngle(newRing2.getAngle() + newRing2.getVelocity());
+        newRing2.setRotAngle(newRing2.getRotAng() + newRing2.getRotVelocity());
+
+        newRing3.setAngle(newRing3.getAngle() + newRing3.getVelocity());
+        newRing3.setRotAngle(newRing3.getRotAng() + newRing3.getRotVelocity());
 
         for(int i = 0; i < saturnsRings.length; i++) {
 
-
             if(saturnsRings[i] == null) {
 
-              
                 saturnsRings[i] = newRing;
-    
-            
-            }
-            saturnsRings[i].moveRotate();
-           
-        }
-        System.out.println(saturnsRings[0].getRotAng());
-        System.out.println(saturnsRings[1].getRotAng());
 
+                if(saturnsRings[200] != null) {
+                    
+                    saturnsRings[i] = newRing2;
+                }
+
+                 if(saturnsRings[400] != null) {
+                    
+                    saturnsRings[i] = newRing3;
+                }
+            }
+        
+         saturnsRings[i].setRotAngle(saturnsRings[i].getRotAng() + saturnsRings[i].getRotVelocity() + 1);
+         theSolarSystem.drawSolarObjectAbout(saturnsRings[i].getDistance(), saturnsRings[i].getAngle(), saturnsRings[i].getDiameter(), saturnsRings[i].getColour(), saturnsRings[i].getRotDis(),  saturnsRings[i].getRotAng());
+
+        }
+
+      
 
         
     }
--- Driver.java
+++ Driver.java
@@ -2,8 +2,6 @@ public class Driver {
     
     public static void main(String[] arguments) {
 
-        int num = 10;
-
         // INSTANCES
 
         SolarSystem nSolar = new SolarSystem(1100, 1100);
@@ -20,22 +18,27 @@ public class Driver {
 
         Planet mars = new Planet(nSolar, theSun, 190, 90,12, "#ce890a", 2.8);
 
-        Planet jupiter = new Planet(nSolar, theSun, 290, 90,42, "#e18909", 2.3);
+        Planet jupiter = new Planet(nSolar, theSun, 260, 90,35, "#e18909", 2.3);
+
+        Moon io = new Moon(nSolar, jupiter, 260, 90, 6, "YELLOW", 23, 90, 2.3, 18);
+        Moon europa = new Moon(nSolar, jupiter, 260, 90, 5, "BROWN", 28, 90, 2.3, 13);
+        Moon ganymede = new Moon(nSolar, jupiter, 260, 90, 9, "GREY", 31, 90, 2.3, 8);
+        Moon callisto = new Moon(nSolar, jupiter, 260, 90, 7, "GREEN", 40, 90, 2.3, 4);
 
         Planet saturn = new Planet(nSolar, theSun, 360, 90,49, "#fffda7", 2);
 
-        Planet uranus = new Planet(nSolar, theSun, 440, 90, 30, "CYAN", 1.7);
+        Planet uranus = new Planet(nSolar, theSun, 490, 90, 30, "CYAN", 1.7);
 
-        Planet neptune = new Planet(nSolar, theSun, 510, 90,35, "#2486db", 1.4);
+        Planet neptune = new Planet(nSolar, theSun, 560, 90,33, "#2486db", 1.4);
 
         Asteroid rogue  = new Asteroid(nSolar, mars, 210, 90, 3, "GREY", 0.5, 0, 23, 0);
 
         Arrays newArray = new Arrays(nSolar, saturn, 0, 0, 0, "GREEN", 0, 0, 0, 0);
 
 
-        Asteroid ring1 = new Asteroid(nSolar, saturn, 360, 90, 3, "RED", 15, 90, 2, 5);
-
-        Asteroid ring2 = new Asteroid(nSolar, saturn, 360, 90, 3, "YELLOW", 15, 90, 2, 7);
+        Asteroid ring1 = new Asteroid(nSolar, saturn, 360, 90, 7, "#7c4104", 1, 40, 2, 200);
+        Asteroid ring2 = new Asteroid(nSolar, saturn, 360, 90, 3, "WHITE", 1, 45, 2, 200);
+        Asteroid ring3 = new Asteroid(nSolar, saturn, 360, 90, 5, "#fe9e3c", 1, 55, 2, 200);
 
        
 
@@ -56,19 +59,23 @@ public class Driver {
             earth.moveObject();
 
 
-            newArray.addAsteroid(rogue);
-            newArray.addRing(ring1);
-            // ring1.moveRotate();
-            // ring2.moveRotate(); 
-          
+
             earthMoon.moveRotate();
 
             mars.moveObject();
+
             jupiter.moveObject();
+            io.moveRotate();
+            europa.moveRotate();
+            ganymede.moveRotate();
+            callisto.moveRotate();
+
             saturn.moveObject();
             uranus.moveObject();
             neptune.moveObject();
 
+            newArray.addAsteroid(rogue);
+            newArray.addRing(ring1, ring2, ring3);
 
 
        }
